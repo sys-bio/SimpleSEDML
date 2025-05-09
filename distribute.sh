00ca2b6e34b6ec568e9e77f78bc8c2aaffc51934
Joseph Hellerstein <jlheller@uw.edu>
2025-05-08 17:24:37 -0700

    Making a package

---
+++ distribute.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# $HOME/.pypirc contains API token information
+# To update the API token, log into PyPI>Account settings, look for
+#   API tokens
+python3 -m build --verbose
+python3 -m twine upload dist/*
