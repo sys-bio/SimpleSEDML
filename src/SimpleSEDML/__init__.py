da04fd6c356cf222ac40ae437db205a1fda35fda
Joseph Hellerstein <jlheller@uw.edu>
2025-05-18 13:20:09 -0700

    Expose API through __init__.

--- src/SimpleSEDML/__init__.py
+++ src/SimpleSEDML/__init__.py
@@ -1,1 +1,6 @@
 __version__ = "0.0.4"
+from SimpleSEDML.simple_sedml import SimpleSEDML # type:ignore
+import SimpleSEDML.constants as cn # type:ignore
+makeSingleModelTimeCourse = SimpleSEDML.makeSingleModelTimeCourse # type:ignore
+makeMultipleModelTimeCourse = SimpleSEDML.makeMultipleModelTimeCourse # type:ignore
+getModelInformation = SimpleSEDML.getModelInformation # type:ignore
