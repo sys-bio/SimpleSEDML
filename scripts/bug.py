9c0bcc04a2e20edf6d618964148ba7ac61fa589e
Joseph Hellerstein <jlheller@uw.edu>
2025-07-03 13:54:13 -0700

    revise example

--- scripts/bug.py
+++ scripts/bug.py
@@ -6,6 +6,6 @@ S1 -> S2; k1*S1
 k1 = 0.4; S1 = 10
 """)
 m = r.simulate (0,50, 100)
-r.plot()
-ss = SimpleSEDML.makeSingleModelTimeCourse(r.getSBML())
+#r.plot()
+ss = SimpleSEDML.makeSingleModelTimeCourse(r.getSBML(), is_plot=False)
 ss.execute()
