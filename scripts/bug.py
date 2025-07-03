9d5003eac28252d3273ce93661c595c0e7424c17
Joseph Hellerstein <jlheller@uw.edu>
2025-07-03 12:40:43 -0700

    Replicate bug

---
+++ scripts/bug.py
@@ -0,0 +1,11 @@
+import tellurium as te
+import SimpleSEDML
+
+r = te.loada("""
+S1 -> S2; k1*S1
+k1 = 0.4; S1 = 10
+""")
+m = r.simulate (0,50, 100)
+r.plot()
+ss = SimpleSEDML.makeSingleModelTimeCourse(r.getSBML())
+ss.execute()
